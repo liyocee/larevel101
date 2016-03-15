607d8c573eb9f5c1367f6148c2105f9d876a12d3
liyocee <collinskivale@gmail.com>
2016-03-15 11:25:17 +0300

    add data seeders

--- laravel_tut/database/seeds/DatabaseSeeder.php
+++ laravel_tut/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard(); //to allow for mass assignment
         // $this->call(UsersTableSeeder::class);
+        $this->call('ArticlesTableSeeder');
+    }
+}
+
+class ArticlesTableSeeder extends Seeder{
+    public function run(){
+        $faker = Faker\Factory::create();
+        DB::table('articles')->delete();
+        $articles = [];
+        for($i=0; $i<100; $i++){
+            $article_dt = $faker->dateTime('now');
+            array_push($articles, [
+                'title'=>$faker->sentence,
+                'body'=>$faker->realText,
+                'published'=>$faker->unixTime(),
+                'author'=>$faker->numberBetween(0, 20),
+                'created_at'=>$article_dt,
+                'updated_at'=>$article_dt
+                ]);
+        }
+        DB::table('articles')->insert($articles);
+        // DB::table('articles')->insert([
+        //     'title'=>'First blog post',
+        //     'body'=>'Something interesting to say',
+        //     'published'=>'1429860898',
+        //     'author'=>1]
+        //     );
     }
 }
